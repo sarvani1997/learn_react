52f00e5f6fe2adf73ca7968fc2e0b5199bce2ae9
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-06-18 22:58:16 +0530

    react portal

--- src/FeedbackPage.tsx
+++ src/FeedbackPage.tsx
@@ -1,8 +1,55 @@
 import { useState } from "react";
+import { createPortal } from "react-dom";
 import "./App.css";
 
+function ModalContent({ onClose }) {
+  return (
+    <div className="modal">
+      <div>I'm a modal dialog</div>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+function NoPortalExample() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>
+        Show modal without a portal
+      </button>
+      {showModal && <ModalContent onClose={() => setShowModal(false)} />}
+    </>
+  );
+}
+
+function PortalExample() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>
+        Show modal using a portal
+      </button>
+      {showModal &&
+        createPortal(
+          <ModalContent onClose={() => setShowModal(false)} />,
+          document.body
+        )}
+    </>
+  );
+}
+
 function FeedbackTab() {
-  return <div>feedback</div>;
+  return (
+    <>
+      <div className="clipping-container">
+        <NoPortalExample />
+      </div>
+      <div className="clipping-container">
+        <PortalExample />
+      </div>
+    </>
+  );
 }
 
 export default FeedbackTab;
