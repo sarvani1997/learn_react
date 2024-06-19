c5f1b5648107fd7ea2da124b662b00670ece4a01
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-06-19 18:23:23 +0530

    make functional feedback modal

--- src/FeedbackPage.tsx
+++ src/FeedbackPage.tsx
@@ -4,51 +4,65 @@ import "./App.css";
 
 function ModalContent({ onClose }) {
   return (
-    <div className="modal">
+    <div className="flex flex-col modal  border border-current">
       <div>I'm a modal dialog</div>
       <button onClick={onClose}>Close</button>
     </div>
   );
 }
 
-function NoPortalExample() {
-  const [showModal, setShowModal] = useState(false);
-  return (
-    <>
-      <button onClick={() => setShowModal(true)}>
-        Show modal without a portal
-      </button>
-      {showModal && <ModalContent onClose={() => setShowModal(false)} />}
-    </>
-  );
-}
-
-function PortalExample() {
-  const [showModal, setShowModal] = useState(false);
-  return (
-    <>
-      <button onClick={() => setShowModal(true)}>
-        Show modal using a portal
-      </button>
-      {showModal &&
-        createPortal(
-          <ModalContent onClose={() => setShowModal(false)} />,
-          document.body
-        )}
-    </>
-  );
-}
-
 function FeedbackTab() {
+  const [showModal, setShowModal] = useState(false);
+  const handleKeyDown = (e) => {
+    // Access the pressed key
+    console.log("key down");
+    console.log({ e });
+    const keyPressed = e.key;
+
+    if (keyPressed === "Escape") {
+      setShowModal(false);
+    }
+
+    console.log({ keyPressed });
+  };
   return (
-    <>
-      <div className="clipping-container">
-        <NoPortalExample />
+    <div onKeyDown={handleKeyDown}>
+      <div
+        onClick={() => showModal && setShowModal(false)}
+        className={`min-h-screen ${
+          showModal ? "bg-gray-400 opacity-50" : ""
+        } z-0`}
+      >
+        <h3>sakjdfnidlkfor</h3>
+        <div>
+          <ol>
+            <li>bdjgda</li>
+            <li>bdjgda</li>
+            <li>bdjgda</li>
+            <li>bdjgda</li>
+            <li>bdjgda</li>
+            <li>bdjgda</li>
+            <li>bdjgda</li>
+            <li>bdjgda</li>
+            <li>bdjgda</li>
+          </ol>
+        </div>
       </div>
-      <div className="clipping-container">
-        <PortalExample />
+      <div className={`absolute right-[-22px] bottom-[100px] rotate-90 z-10`}>
+        <div className="border-2 p-4 border-black ">
+          <>
+            <button className="" onClick={() => setShowModal(!showModal)}>
+              Feedback
+            </button>
+            {showModal &&
+              createPortal(
+                <ModalContent onClose={() => setShowModal(!showModal)} />,
+                document.body
+              )}
+          </>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
